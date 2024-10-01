20539061fa8052c0f96db14ccd274ab6aa07fa95
Stiffpixels <saifimuzammil2@gmail.com>
2024-10-01 13:33:49 +0530

    changes to the routing structure

---
+++ src/app/messenger/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Chat } from "@/components/chats/Chat"
+
+export default function Chats() {
+    return (
+        <main className="mx-auto w-11/12">
+            <Chat />
+        </main>
+    )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,12 @@ import { ThemeSwitch } from "./context/ThemeSwitch"
 export function Navbar() {
     return (
         <>
-            <nav className="block py-4 mx-auto w-11/12">
+            <nav className="sticky py-4 mx-auto w-11/12 backdrop-blur-sm">
                 <ul className="flex justify-between items-center">
                     <li>
                         <h2 className="text-amber-500 text-2xl font-semibold">WWC</h2>
                     </li>
+
                     <div className="flex gap-2 items-center">
                         <li className="flex gap-2 items-center">
                             <Input placeholder="search" className="p-2 h-auto" />
@@ -17,6 +18,7 @@ export function Navbar() {
                             <ThemeSwitch/>
                             </div>
                         </li>
+
                     </div>
                 </ul>
             </nav>
