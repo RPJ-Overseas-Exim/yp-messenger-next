accbc054abbe3e863f7ca11b7009136fe06fc294
Stiffpixels <saifimuzammil2@gmail.com>
2024-10-01 12:49:07 +0530

    added darkmode and lightmode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,23 @@
 import React from "react"
-import { EllipsisVertical } from "lucide-react"
+import { Input } from "./ui/input"
+import { ThemeSwitch } from "./context/ThemeSwitch"
 
-export const Navbar = () => {
+export function Navbar() {
     return (
         <>
             <nav className="block py-4 mx-auto w-11/12">
-                <ul className="flex justify-between">
+                <ul className="flex justify-between items-center">
                     <li>
-                        <h2 className="text-amber-500 text-xl font-semibold">WCC</h2>
+                        <h2 className="text-amber-500 text-2xl font-semibold">WWC</h2>
                     </li>
-                    <li>
-                        <EllipsisVertical />
-                    </li>
+                    <div className="flex gap-2 items-center">
+                        <li className="flex gap-2 items-center">
+                            <Input placeholder="search" className="p-2 h-auto" />
+                            <div className="rounded-full active:bg-secondary">
+                            <ThemeSwitch/>
+                            </div>
+                        </li>
+                    </div>
                 </ul>
             </nav>
             <hr />
