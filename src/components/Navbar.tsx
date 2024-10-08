db892bb56f8da8dbadaf8b4397ee1dc6550bc0ea
Stiffpixels <saifimuzammil2@gmail.com>
2024-10-08 11:13:01 +0530

    updated revalidation so that everytime user leaves chat it revalidates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,26 @@
 "use client"
-import React from "react"
+import React, { useEffect } from "react"
 import { Input } from "./ui/input"
 import { ThemeSwitch } from "./context/ThemeSwitch"
 import Link from "next/link"
-import { useSearchParams, usePathname } from "next/navigation"
+import { useSearchParams, usePathname, useParams } from "next/navigation"
+import { useAtomValue } from "jotai"
+import { socketAtom } from "@/lib/jotai/atoms"
 
 export function Navbar() {
     const pathname = usePathname()
-    const params = useSearchParams()
+    const searchParams = useSearchParams()
+    const params = useParams()
+    const socket = useAtomValue(socketAtom)
     const chatCondition = (pathname !== "/messenger"
         && pathname !== "/messenger/profile"
         && pathname !== "/messenger/broadcast")
+
+    useEffect(() => {
+        if (!params?.chatId) {
+            socket?.emit("leaveChat", params?.chatId as string || "")
+        }
+    }, [params?.chatId])
 
     return (
         <div className="sticky backdrop-blur-sm">
@@ -20,7 +30,7 @@ export function Navbar() {
                         <Link href="/messenger">
                             <h2 className="text-amber-500 text-2xl font-semibold">
                                 {chatCondition ? (
-                                    params?.get("name")
+                                    searchParams?.get("name")
                                 ) : (
                                     "WWC"
                                 )}
