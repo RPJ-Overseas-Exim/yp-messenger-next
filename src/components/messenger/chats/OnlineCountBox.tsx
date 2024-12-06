65111ded0fb00780b75b64ecb3217a437b458cbe
Vikrant <tiwarivikrant9764@gmail.com>
2024-12-06 15:15:03 +0530

    hide the online count from the user page

--- src/components/messenger/chats/OnlineCountBox.tsx
+++ src/components/messenger/chats/OnlineCountBox.tsx
@@ -1,11 +1,18 @@
 "use client"
+import { roleAtom } from "@/lib/jotai/atoms";
+import { useAtomValue } from "jotai";
 import React from "react"
 
 export default function OnlineCountBox({onlineCount}: {onlineCount: number;}){
+    const role = useAtomValue(roleAtom)
     
     return (
-        <div className="border border-border text-green-600 text-center font-bold whitespace-nowrap fixed bg-background p-2 right-10 bottom-20 rounded-lg">
-            {onlineCount} online
-        </div>
+        <>
+        {role === "admin" && (
+            <div className="border border-border text-green-600 text-center font-bold whitespace-nowrap fixed bg-background p-2 right-10 bottom-20 rounded-lg">
+                {onlineCount} online
+            </div>
+        )}
+        </>
     )
 }
